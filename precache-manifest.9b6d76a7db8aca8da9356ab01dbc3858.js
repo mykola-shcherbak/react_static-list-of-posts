e170870cdf200cf42a3b1a70349ec73a86ebe296
Mykola shcherbak <hohotun21121987@gmail.com>
2021-07-20 17:44:44 +0300

    Updates

---
+++ precache-manifest.9b6d76a7db8aca8da9356ab01dbc3858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc62ad4d7d42a33b72f005987be4480",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c85baeb8889a44a8c774",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "d36cf7d1584e4e69c9e6",
+    "url": "./static/js/2.0472d363.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0472d363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85baeb8889a44a8c774",
+    "url": "./static/js/main.d3b95687.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
